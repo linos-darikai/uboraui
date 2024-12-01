6bdd6e6200dc59a0d9f81aa9c3af363db3a8cac8
Avalance546 <138253858+Avalance546@users.noreply.github.com>
2024-12-01 19:08:42 +0000

    Made the HomePage added the movement functionality

--- src/main/java/linos/HomePage.java
+++ src/main/java/linos/HomePage.java
@@ -1,5 +1,26 @@
 package linos;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class HomePage{
-    
+    @FXML
+    private Label categoryName;
+    @FXML
+    void backToLogin(MouseEvent event) throws Exception {
+       
+        Parent root = (new FXMLLoader(getClass().getResource("login_page.fxml"))).load();
+        Stage stage = (Stage)((Node)(event.getSource())).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
 }
--- src/main/java/linos/LoginPage.java
+++ src/main/java/linos/LoginPage.java
@@ -1,6 +1,8 @@
 package linos;
 
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -42,7 +44,14 @@ public class LoginPage {
     
 
     @FXML
-    void login(ActionEvent event) {
+    void login(ActionEvent event)throws IOException {
+    
+        Parent root = (new FXMLLoader(getClass().getResource("home_page.fxml"))).load();
+        Stage stage = (Stage)((Node)(event.getSource())).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
 
     }
 
